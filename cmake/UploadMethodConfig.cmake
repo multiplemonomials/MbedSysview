8f4ab57f6e7e239107690398a4c1eb83b339332e
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2020-12-13 15:55:12 -0800

    Update for new upload method names

--- cmake/UploadMethodConfig.cmake
+++ cmake/UploadMethodConfig.cmake
@@ -21,7 +21,7 @@ set(JLINK_UPLOAD_ENABLED FALSE)
 
 if(JLINK_UPLOAD_ENABLED)
     set(JLINK_CPU_NAME STM32F429ZI)
-    set(JLINK_JTAG_SPEED 4000)
+    set(JLINK_CLOCK_SPEED 4000)
 endif()
 
 # Config options for PYOCD
@@ -31,7 +31,7 @@ set(PYOCD_UPLOAD_ENABLED TRUE)
 
 if(PYOCD_UPLOAD_ENABLED)
     set(PYOCD_TARGET_NAME stm32f429xi)
-    set(PYOCD_JTAG_SPEED 4000k)
+    set(PYOCD_CLOCK_SPEED 4000k)
 endif()
 
 # Config options for OPENOCD
@@ -63,14 +63,7 @@ set(STM32CUBE_UPLOAD_ENABLED TRUE)
 
 if(STM32CUBE_UPLOAD_ENABLED)
 
-    # create option to select the probe to connect to
-    set(STM32CUBE_PROBE_SN "" CACHE STRING "Serial number of the ST-Link probe to connect to.  If blank, will connect to any probe.")
-
     set(STM32CUBE_CONNECT_COMMAND -c port=SWD reset=HWrst)
     set(STM32CUBE_GDBSERVER_ARGS --swd)
 
-    if(NOT "${STM32CUBE_PROBE_SN}" STREQUAL "")
-        list(APPEND STM32CUBE_CONNECT_COMMAND sn=${STM32CUBE_PROBE_SN})
-        list(APPEND STM32CUBE_GDBSERVER_ARGS --serial-number ${STM32CUBE_PROBE_SN})
-    endif()
 endif()
